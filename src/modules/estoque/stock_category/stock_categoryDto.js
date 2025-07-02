e4845061eeadaf4043a4b1ec7f1b000ba745f80e
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-02 09:50:35 -0300

    feat(StockCategory): criação do DTO
    Criação do DTO de StockCategory

--- src/modules/estoque/stock_category/stock_categoryDto.js
+++ src/modules/estoque/stock_category/stock_categoryDto.js
@@ -1,1 +1,16 @@
-//Criar depois
+//stock_categoryDto.js
+const Joi = require('joi');
+
+const createStockCategorySchema = Joi.object({
+  name: Joi.string().min(3).max(50).required(),
+  status: Joi.boolean().default(true).required()
+});
+
+const updateStockCategorySchema = Joi.object({
+  name: Joi.string().min(3).max(50).optional(),
+  status: Joi.boolean().optional()
+});
+
+module.exports = { 
+  createStockCategorySchema, 
+  updateStockCategorySchema };
