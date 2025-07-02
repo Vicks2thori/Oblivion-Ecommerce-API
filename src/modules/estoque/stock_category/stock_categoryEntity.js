8767e77e092f35278389cdf6b02502360f9d43ea
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-02 09:54:57 -0300

    refactor(Entity): Schema base StockCategory
    Criei a principio o schema no qual vou me basear para a criação de tabelas, principalmente as que não possuem relacionamento.

--- src/modules/estoque/stock_category/stock_categoryEntity.js
+++ src/modules/estoque/stock_category/stock_categoryEntity.js
@@ -1,54 +1,19 @@
 //stock_category.js
-const pool = require('../../../model/conection_db');
+const mongoose = require('mongoose');
 
-//Create
-async function createStockCategory({name, status}) {
-  const [result] = await pool.query(`
-    INSERT INTO stock_category (name, status)
-    VALUES (?, ?)`, [name, status]
-  );
-  return result.insertId;
-}
-
-//Read
-async function getStockCategory({name}) {
-  const [rows] = await pool.query(`
-    SELECT * FROM stock_category WHERE name = ?`, [name]
-  );
-  return rows[0];
-}
-
-//Update
-async function updateStockCategory({id, name, status}) {
-  // Filtra apenas campos que foram enviados (não undefined/null)
-  const fieldsToUpdate = {};
-  
-  if (name) fieldsToUpdate.name = name;
-  if (status !== undefined) fieldsToUpdate.status = status; // Boolean pode ser false
-
-  if (Object.keys(fieldsToUpdate).length === 0) {
-    return { success: false, message: 'Nenhum campo para atualizar' };
+const StockCategorySchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: [true, 'Nome é obrigatório'], //required + mensagem personalizada
+    trim: true,  //Remove espaços inicio/fim
+    minlength: [3, 'Nome deve ter um minímo de 3 caracteres'],
+    maxlength: [50, 'Nome deve ter um máximo de 50 caracteres']
+  },
+  status: { 
+    type: Boolean,
+    required: true,
+    default: true 
   }
+}, { timestamps: true });
 
-  // Constrói query dinâmica
-  const fields = Object.keys(fieldsToUpdate);
-  const values = Object.values(fieldsToUpdate);
-  const setClause = fields.map(field => `${field} = ?`).join(', ');
-
-  const [result] = await pool.query(`
-    UPDATE stock_category
-    SET ${setClause}
-    WHERE id = ?
-    LIMIT 1
-  `, [...values, id]); //... "espalha" os arrays
-
-  return {
-    success: result.affectedRows > 0,
-    affectedRows: result.affectedRows
-  };
-}
-
-module.exports = { 
-  createStockCategory, 
-  getStockCategory, 
-  updateStockCategory };
+module.exports = mongoose.model('Category', StockCategorySchema);
