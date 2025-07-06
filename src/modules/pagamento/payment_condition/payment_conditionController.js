9ee35a5375a7f5146b7458d970d04dde9219837d
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-06 12:44:26 -0300

    feat(payment_conditionController): Tratamento de erros
    Comentei algumas questões que poderiam me gerar duvida, a principio vou registrar aqui os erros mais comuns, mas vou criar um arquivo separado para isso e alterar.

--- src/modules/pagamento/payment_condition/payment_conditionController.js
+++ src/modules/pagamento/payment_condition/payment_conditionController.js
@@ -1,43 +1,90 @@
-const { createPaymentConditionSchema, updatePaymentConditionSchema } = require('./paymentConditionDto');
-const PaymentCondition = require('./paymentConditionEntity');
+//payment_conditionController.js
+const { createPaymentConditionSchema, updatePaymentConditionSchema } = require('./payment_conditionDto');
+const PaymentCondition = require('./payment_conditionService');
 
 //CRUD
 
 //Create
 async function create(req, res) {
   try {
-    // 1. Validar DTO
-    const { error, value } = createPaymentConditionSchema.validate(req.body);
+    //Validar DTO
+    const { error, value } = createPaymentConditionSchema.validate(req.body); //validate do Joi retorna um erro(null se estiver ok) e os valores
     if (error) {
+      //400 - Dados inválidos
       return res.status(400).json({
         success: false,
-        message: 'Dados inválidos',
-        errors: error.details.map(d => d.message)
+        message: '400 - Dados inválidos',
+        errors: error.details.map(d => d.message) //extrai só as mensagens
       });
-    }
+    };
 
-    // 2. Criar através da Entity
+    //Criar através do Service
     const paymentCondition = await PaymentCondition.createPaymentCondition(value);
 
-    // 3. Resposta padronizada
-    res.status(201).json({
+
+
+    //RESPOSTAS PADRONIZADAS (400 o Joi valida)
+
+    //OK
+    //200 - Sucesso geral
+    res.status(200).json({
       success: true,
-      message: 'Condição de pagamento criada com sucesso',
+      message: '200 - Operação realizada com sucesso',
       data: paymentCondition
     });
 
-  } catch (error) {
+    //201 - Criado com sucesso
+    res.status(201).json({
+      success: true,
+      message: '201 - Condição de pagamento criada com sucesso',
+      data: paymentCondition
+    });
+
+  }catch (error) {
+
+    //ERROR
+    //401 - Não autenticado (TEMPORÁRIO até a criação do middleware)
+    res.status(401).json({
+      success: false,
+      message: '401 - Não autenticado'
+    });
+
+    //403 - Não autorizado (TEMPORÁRIO até a criação do middleware)
+    res.status(403).json({
+      success: false,
+      message: '403 - Não autorizado'
+    });
+
+    //404 - Não encontrado
+    res.status(404).json({
+      success: false,
+      message: '404 - Não encontrado'
+    });
+
+    //409 - Conflito
+    res.status(409).json({
+      success: false,
+      message: '409 - Conflito com recurso existente'
+    });
+
+    //422 - Entidade não processavel
+    res.status(404).json({
+      success: false,
+      message: '422 - Não foi possivel processar requisição'
+    });
+
+    //500 - Erro interno do servidor
     res.status(500).json({
       success: false,
-      message: error.message
+      message: '500 - Erro interno do servidor'
     });
-  }
-}
+  };
+};
 
 
 //Read
 
-//Get all
+//All
 async function getAll(req, res) {
   try {
     const paymentConditions = await PaymentCondition.getAllPaymentConditions();
@@ -55,7 +102,25 @@ async function getAll(req, res) {
   }
 }
 
-//Get by id
+//Active
+async function getActive(req, res) {
+  try {
+    const activeConditions = await PaymentCondition.getActivePaymentConditions();
+
+    res.json({
+      success: true,
+      data: activeConditions
+    });
+
+  }catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+//By ID
 async function getById(req, res) {
   try {
     const { id } = req.params;
@@ -66,13 +131,13 @@ async function getById(req, res) {
       data: paymentCondition
     });
 
-  } catch (error) {
+  }catch (error) {
     res.status(404).json({
       success: false,
       message: error.message
     });
-  }
-}
+  };
+};
 
 
 //Update
