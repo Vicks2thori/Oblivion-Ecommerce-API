ac778c6a14f6c16b02972913633c9ce73350b491
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-12 12:52:48 -0300

    refac(User: Entity e Dto): Embedding subdocumentos

--- src/modules/usuários/user/userDto.js
+++ src/modules/usuários/user/userDto.js
@@ -1,19 +1,51 @@
 //userDto.js
 const Joi = require('joi');
 
-const createUserSchema = Joi.object({
+const createAdminSchema = Joi.object({ //criei um para cada ja que se cria e se atualiza separadamente
   name: Joi.string().min(5).max(80).required(),
-  email: Joi.string().email().min(6).max(50).required(), //unique
+  email: Joi.string().email().min(6).max(50).required(),
   password: Joi.string().min(8).max(255).required(),
-  type: Joi.string().valid('client', 'admin').required()
-});
+  type: Joi.string().valid('admin').required(),
+  adminDetails: Joi.object({
+    status: Joi.boolean().default(true)
+  }).required()
+}).min(6).max(6);
 
-const updateUserSchema = Joi.object({
+const createClientSchema = Joi.object({
+  name: Joi.string().min(5).max(80).required(),
+  email: Joi.string().email().min(6).max(50).required(),
+  password: Joi.string().min(8).max(255).required(),
+  type: Joi.string().valid('client').required(),
+  clientDetails: Joi.object({
+    cpf: Joi.string().length(11).required(),
+    cell: Joi.string().length(11).required()
+  }).required()
+}).min(5).max(5); //clientDetails conta como 1 objeto
+
+const updateAdminSchema = Joi.object({
   name: Joi.string().min(5).max(80).optional(),
-  email: Joi.string().email().min(6).max(50).optional(), //unique
-  password: Joi.string().min(8).max(255).optional()
-});
+  email: Joi.string().email().min(6).max(50).optional(),
+  password: Joi.string().min(8).max(255).optional(),
+  type: Joi.string().valid('admin').required(), //para validar se pode alterar
+  adminDetails: Joi.object({
+    status: Joi.boolean().default(true)
+  }).optional()
+}).min(2).max(5); //minimo 2 pois precisa do tipo + 1 alteração
+
+const updateClientSchema = Joi.object({
+  name: Joi.string().min(5).max(80).optional(),
+  email: Joi.string().email().min(6).max(50).optional(),
+  password: Joi.string().min(8).max(255).optional(),
+  type: Joi.string().valid('client').required(), //para validar se pode alterar
+  clientDetails: Joi.object({
+    cpf: Joi.string().length(11).optional(),
+    cell: Joi.string().length(11).optional()
+  }).optional()
+}).min(2).max(5);
 
 module.exports = { 
-  createUserSchema, 
-  updateUserSchema};
+  createAdminSchema,
+  createClientSchema,
+  updateClientSchema,
+  updateAdminSchema
+};
