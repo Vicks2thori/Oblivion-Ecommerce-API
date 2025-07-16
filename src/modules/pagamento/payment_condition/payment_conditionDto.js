fa1b6a0be92be71ae6974039e7c179103a298c6a
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-16 09:35:09 -0300

    feat(payment_conditionDto): Adicionei quantidade max e min de req
    Para a validação ser eficiente adicionei a quantidade minima e maxima de 2 na criação (pois preciso dessas duas informações). E minima de 1 e maxima de 3 no update (que estou pensando se faz sentido). No caso do update gostaria de deixar no máximo 2, pois no caso do deleted seria interessante apenas receber ele do front ao invés de todos os dados alterados (e por fim deletados, que não faz sentido).

--- src/modules/pagamento/payment_condition/payment_conditionDto.js
+++ src/modules/pagamento/payment_condition/payment_conditionDto.js
@@ -7,13 +7,13 @@ const createPaymentConditionSchema = Joi.object({
   name: Joi.string().min(2).max(50).required(),
   status: Joi.boolean().default(true).required(),
   //deleted não vem do front é controlado no back (pelo menos na hora de criar)
-});
+}).min(2).max(2);
 
 const updatePaymentConditionSchema = Joi.object({
   name: Joi.string().min(2).max(50).optional(),
   status: Joi.boolean().optional(),
   deleted: Joi.boolean().optional()
-});
+}).min(1).max(3);
 
 module.exports = { 
   createPaymentConditionSchema, 
