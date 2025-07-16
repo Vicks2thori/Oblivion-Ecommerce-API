881366eab10d2a1151e71a8f16db7583121fd85c
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-16 12:51:17 -0300

    feat(paymentDto): Criação do Schema Create e Update
    Optei por utilizar os atributos string().length(24).hex() para validar o id em contrapartida do joi-objectId. Fiz isso pois a vantagem de se usar o joi-objectId era mais sobre legibilidade, enquanto precisaria instalar mais um pacote.

--- src/modules/pagamento/payment/paymentDto.js
+++ src/modules/pagamento/payment/paymentDto.js
@@ -3,17 +3,27 @@ const Joi = require('joi');
 
 const createPaymentSchema = Joi.object({
   name: Joi.string().min(3).max(50).required(),
-  image_id: Joi.number().min(1).max(255).required(),
-  condition_id: Joi.number().min(1).max(255).required(),
-  statusPayment: Joi.boolean().default(true).required()
-});
+  imageType: Joi.string().valid('Pix', 'Others', 'Money', 'Card', 'Voucher').required(),
+  paymentConditions: Joi.array().items(
+    Joi.object({
+      conditionsId: Joi.string().length(24).hex().required() //um id no mango é uma string de 24 digitos hexadecimal
+    })
+  ).min(1).required(),
+  status: Joi.boolean().default(true).required()
+}).min(3); //ainda estou avaliando sobre a quantidade minima (condition vai ser obrigatório?)
+//máximo tambem, ja que vai ter várias condições
 
 const updatePaymentSchema = Joi.object({
   name: Joi.string().min(3).max(50).optional(),
-  image_id: Joi.number().min(1).max(255).optional(),
-  condition_id: Joi.number().min(1).max(255).optional(),
-  statusPayment: Joi.boolean().optional()
-});
+  imageType: Joi.string().valid('Pix', 'Others', 'Money', 'Card', 'Voucher').optional(),
+  paymentConditions: Joi.array().items( //para a validação de muitas condições de pagamento
+    Joi.object({
+      conditionsId: Joi.string().length(24).hex().optional()
+    })
+  ).min(1).optional(),
+  status: Joi.boolean().optional(),
+  deleted: Joi.boolean().optional()
+}).min(1);
 
 module.exports = { 
   createPaymentSchema, 
