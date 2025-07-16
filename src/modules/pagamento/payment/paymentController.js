0b0b5ee93f34ba83d656a33cd9222c62355bad34
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-16 14:50:59 -0300

    feat(paymentController): Criação do CRUD nocontroller

---
+++ src/modules/pagamento/payment/paymentController.js
@@ -0,0 +1,169 @@
+//payment_conditionController.js
+const { createPaymentSchema, updatePaymentSchema } = require('./paymentDto');
+const Payment = require('./paymentService');
+
+//CRUD
+
+//Create
+async function create(req, res) {
+  try {
+    //Validar DTO
+    const { error, value } = createPaymentSchema.validate(req.body); //validate do Joi retorna um erro(null se estiver ok) e os valores
+    if (error) {
+      //400 - Dados inválidos
+      return res.status(400).json({
+        success: false,
+        message: '400 - Dados inválidos',
+        errors: error.details.map(d => d.message) //extrai só as mensagens
+      });
+    };
+
+    //Criar através do Service
+    const payment = await Payment.createPayment(value);
+
+    //200 - Sucesso geral
+    res.status(200).json({
+      success: true,
+      message: '200 - Operação realizada com sucesso',
+      data: payment
+    });
+
+
+  }catch (error) {
+    //500 - Erro interno do servidor
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  };
+};
+
+
+//Read
+
+//All
+async function getAll(req, res) {
+  try {
+    const payment = await Payment.getAllPayments();
+
+    //OK
+    //200 - Sucesso geral
+    res.status(200).json({
+      success: true,
+      message: '200 - Operação realizada com sucesso',
+      data: payment
+    });
+
+  } catch (error) {
+    //500 - Erro interno do servidor
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+}
+
+//Active
+async function getActive(req, res) {
+  try {
+    const activePayments = await Payment.getActivePayments();
+
+    //200 - Sucesso geral
+    res.status(200).json({
+      success: true,
+      message: '200 - Operação realizada com sucesso',
+      data: activePayments
+    });
+
+  }catch (error) {
+    //500 - Erro interno do servidor
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+//By ID
+async function getById(req, res) {
+  try {
+    const { id } = req.params;
+    const payment = await Payment.getPaymentById(id);
+
+    //200 - Sucesso geral
+    res.status(200).json({
+      success: true,
+      message: '200 - Operação realizada com sucesso',
+      data: payment
+    });
+
+  }catch (error) {
+    //500 - Erro interno do servidor
+    return res.status(500).json({
+      success: false,
+      message: error.message 
+    });
+  };
+};
+
+
+//Update
+async function update(req, res) {
+  try {
+    const { id } = req.params;
+
+    const { error, value } = updatePaymentSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        errors: error.details.map(d => d.message) // ✅ Só aqui usar .details
+      });
+    }
+    
+    const result = await Payment.updatePayment(id, value);
+    
+    //200 - Sucesso geral
+    return res.status(200).json({
+      success: true,
+      data: result
+    });
+    
+  } catch (error) {
+    //500 - Erro interno do servidor
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+}
+
+//Delete
+async function deletePayment(req, res) {
+  try {
+    const { id } = req.params;
+    const deleted = await Payment.deletePayment(id);
+
+    //200 - Sucesso geral
+    res.status(200).json({
+      success: true,
+      message: '200 - Operação realizada com sucesso',
+      data: deleted
+    });
+
+  } catch (error) {
+    //500 - Erro interno do servidor
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+}
+
+module.exports = {
+  create,
+  getAll,
+  getActive,
+  getById,
+  update,
+  deletePayment
+};
