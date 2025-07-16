35fd1eb63fb43877c9feaec13fde46888a450d32
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-16 14:52:19 -0300

    feat(paymentRouter): Rotas funcionando (adm e client)

--- src/routes/privateRoutes.js
+++ src/routes/privateRoutes.js
@@ -1,11 +1,13 @@
 //privateRoutes.js  
 const express = require('express');
 const paymentConditionRoutes = require('../modules/pagamento/payment_condition/payment_conditionRouter');
+const paymentRoutes = require("../modules/pagamento/payment/paymentRouter");
 
 const privateRouter = express.Router();
 
 //PAYMENT
 //payment condition
 privateRouter.use('/payment-conditions', paymentConditionRoutes.private);
+privateRouter.use('/payment', paymentRoutes.private)
 
 module.exports = privateRouter;
--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,11 +1,13 @@
 // routes/publicRoutes.js
 const express = require('express');
 const paymentConditionRoutes = require('../modules/pagamento/payment_condition/payment_conditionRouter');
+const paymentRoutes = require("../modules/pagamento/payment/paymentRouter");
 
 const publicRouter = express.Router();
 
 //PAYMENT
 //payment condition
 publicRouter.use('/payment-conditions', paymentConditionRoutes.public);
+publicRouter.use('/payment', paymentRoutes.public);
 
 module.exports = publicRouter;
