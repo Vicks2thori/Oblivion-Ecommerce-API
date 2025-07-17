a4815cb5f996bded36b05b6a2b90dc8743ec1176
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-17 07:07:45 -0300

    feat(categoryDto): Adicionado numero de requisições min e max

--- src/modules/produto/category/categoryDto.js
+++ src/modules/produto/category/categoryDto.js
@@ -4,14 +4,14 @@ const Joi = require('joi');
 const createCategorySchema = Joi.object({
   name: Joi.string().min(3).max(50).required(),
   status: Joi.boolean().default(true).required(),
-  deleted: Joi.boolean().default(false).required()
-});
+  //não se cria deleted isso é o backend que faz
+}).min(2).max(2);
 
 const updateCategorySchema = Joi.object({
   name: Joi.string().min(3).max(50).optional(),
   status: Joi.boolean().optional(),
   deleted: Joi.boolean().optional()
-});
+}).min(1).max(3);
 
 module.exports = { 
   createCategorySchema, 
