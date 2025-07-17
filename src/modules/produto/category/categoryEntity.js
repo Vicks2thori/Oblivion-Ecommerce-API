b6b3aac45fedbe197d09f95ec9a016af56d4ddc0
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-17 07:01:03 -0300

    refactor(categoryEntity): Migração do CRUD e detalhes
    Apaguei o CRUD do Entity, logo mais irei adicionar ao Service, este é só a base do category visto que ainda vou criar os relacionamentos com os produtos (Referencing subdocumentos).

--- src/modules/produto/category/categoryEntity.js
+++ src/modules/produto/category/categoryEntity.js
@@ -8,6 +8,7 @@ const CategorySchema = new mongoose.Schema({
     trim: true,  //Remove espaços inicio/fim
     minlength: [3, 'Nome deve ter um minímo de 3 caracteres'],
     maxlength: [50, 'Nome deve ter um máximo de 50 caracteres']
+    //unique: true, //Quando o unique esta ativo ele retorna um erro, mesmo quando o item foi "deletado"
   },
   status: { 
     type: Boolean,
@@ -19,77 +20,13 @@ const CategorySchema = new mongoose.Schema({
     required: true,
     default: false
   }
-}, { timestamps: true });
+}, { 
+  timestamps: true, //controle automático de tempo
+  versionKey: false //remove campo inutil
+});
 
-//CRUD
-
-//Create
-CategorySchema.statics.createCategory = async function(data) {
-  const category = new this(data);
-  return await category.save();
-};
-
-
-//Read
-//All
-CategorySchema.statics.getAllCategory = async function() {
-  return await this.find({ delete: false }).sort({ name: 1 });
-};
-
-//by ID
-CategorySchema.statics.getCategoryById = async function(id) {
-  const category = await this.findById({
-    id: id,
-    deleted: false //não retornar itens deletados
-  });
-  if (!category) {
-    throw new Error('Categoria não encontrada');
-  }
-  return category;
-};
-
-
-//Update
-CategorySchema.statics.updateCategory = async function(id, validatedData) {
-  try {
-    const updated = await this.findOneAndUpdate(
-      { 
-        _id: id, 
-        isDeleted: false  //só atualiza se não foi deletado
-      },
-      validatedData, 
-      { 
-        new: true, 
-        runValidators: true 
-      }
-    );
-    
-    if (!updated) {
-      throw new Error('Categoria não encontrada');
-    }
-    
-    return updated;
-  } catch (error) {
-    throw new Error(`Erro ao atualizar categoria: ${error.message}`);
-  }
-};
-
-
-//Delete (soft delete)
-CategorySchema.statics.deleteCategory = async function(id) {
-  const deleted = await this.findByIdAndUpdate({
-    id: id,
-    deleted: false
-    }, 
-    { deleted: true }, 
-    { new: true }
-  );
-  
-  if (!deleted) {
-    throw new Error('Categoria não encontrada');
-  }
-  
-  return deleted;
-};
+//indexação para performance
+CategorySchema.index({name: 1})
+CategorySchema.index({status: 1, deleted: 1}) //melhorar
 
 module.exports = mongoose.model('Category', CategorySchema);
