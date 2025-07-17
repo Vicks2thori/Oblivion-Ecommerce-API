1980221a2837ea2479db223aa78fc96aebf735cd
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-17 10:41:04 -0300

    refactor(productDto): Adoção de convenção em atributos
    Alterei o nome do image visto que vou incrementar apenas a Url na entidade ao invés de relacionar. Alterei o nome e o tipo da categoria que relaciona ao produto. E a ordem dos atributos para ficar semelhante ao Entity.

--- src/modules/produto/product/productDto.js
+++ src/modules/produto/product/productDto.js
@@ -3,24 +3,24 @@ const Joi = require('joi');
 
 const createProductSchema = Joi.object({
   name: Joi.string().min(1).max(100).required(),
-  image_id: Joi.number().min(1).max(65535).optional(),
+  imageUrl: Joi.string().min(1).max(255).required(), //Imagem vai ser obrigatória? se sim devo restringir, se não eu preciso ver como ficaria o front
   description: Joi.string().max(65535).optional(),
   price: Joi.number().precision(2).min(0.01).max(999999.99).required(),
   code: Joi.number().min(1).max(9223372036854775807).required(),
-  status: Joi.boolean().default(true).required(),
-  category_id: Joi.number().min(1).max(255).required(),
-  quantity: Joi.number().min(1).max(65535).required()
+  categoryId: string().length(24).hex().optional(),
+  quantity: Joi.number().min(1).max(65535).required(),
+  status: Joi.boolean().default(true).required()
 })
 
 const updateProductSchema = Joi.object({
   name: Joi.string().min(1).max(100).optional(),
-  image_id: Joi.number().min(1).max(65535).optional(),
+  imageUrl: Joi.string().min(1).max(255).optional(),
   description: Joi.string().max(65535).optional(),
   price: Joi.number().precision(2).min(0.01).max(999999.99).optional(),
   code: Joi.number().min(1).max(9223372036854775807).optional(),
-  status: Joi.boolean().optional(),
-  category_id: Joi.number().min(1).max(255).optional(),
-  quantity: Joi.number().min(1).max(65535).optional()
+  categoryId: Joi.string().length(24).hex().optional(),
+  quantity: Joi.number().min(1).max(65535).optional(),
+  status: Joi.boolean().optional()
 })
 module.exports = { 
   createProductSchema, 
