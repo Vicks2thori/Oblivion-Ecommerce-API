121c389cc5b17c3409219b724787b882521467be
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-18 16:21:32 -0300

    refactor(enterpriseDto): nomeclatura e min e max no Schema
    Mudei o nome da imagem, e adcionei validação minima e maxima nos schemas.

--- src/modules/empresa/enterprise/enterpriseDto.js
+++ src/modules/empresa/enterprise/enterpriseDto.js
@@ -3,21 +3,21 @@ const Joi = require('joi');
 
 const createEnterpriseSchema = Joi.object({
   name: Joi.string().min(3).max(50).required(),
-  logo_image: Joi.string().min(5).max(100).optional(), //chave estrangeira deve ser validada no controler
+  logoUrl: Joi.string().min(5).max(255).required(), //vai ser obrigatório?
   phone: Joi.string().length(11).required(),
   instagram: Joi.string().min(1).max(30).optional(),
   facebook: Joi.string().min(5).max(50).optional(),
   email: Joi.string().email().min(6).max(50).optional()
-});
+}).min(3).max(6);
 
 const updateEnterpriseSchema = Joi.object({
   name: Joi.string().min(3).max(50).optional(),
-  logo_image: Joi.number().min(5).max(100).optional(), //chave estrangeira deve ser validada no controler
+  logoUrl: Joi.string().min(5).max(255).optional(),
   phone: Joi.string().length(11).optional(),
   instagram: Joi.string().min(1).max(30).optional(),
   facebook: Joi.string().min(5).max(50).optional(),
   email: Joi.string().email().min(6).max(50).optional()
-});
+}).min(1).max(6);
 
 module.exports = { 
   createEnterpriseSchema, 
