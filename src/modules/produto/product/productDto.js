e9b37aa9e795e2d3263d9bf3a8a30ab9e97ac789
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-18 01:05:16 -0300

    refactor(productDto): Corrigi CreateSchema e adicionei min e max requisições (duvida)
    No Schema antigo categoria estava como opcional na criação sendo que ela é obrigatória,.
    Sobre as requisições validei do status e na teoria o default se não recebe nada ele diz que é true, mas como é obrigatório o Joi valida assim mesmo (não testei só o entity vou testar). Preciso arrumar isso.
    Da mesma forma o deleted mas tambem é uma duvida sobre a criação de rotas.

--- src/modules/produto/product/productDto.js
+++ src/modules/produto/product/productDto.js
@@ -7,10 +7,10 @@ const createProductSchema = Joi.object({
   description: Joi.string().max(65535).optional(),
   price: Joi.number().precision(2).min(0.01).max(999999.99).required(),
   code: Joi.number().min(1).max(9223372036854775807).required(),
-  categoryId: Joi.string().length(24).hex().optional(),
+  categoryId: Joi.string().length(24).hex().required(),
   quantity: Joi.number().min(1).max(65535).required(),
-  status: Joi.boolean().default(true).required()
-})
+  status: Joi.boolean().default(true).required() //se o status é obrigatório mas o default é true como o front manda? e sim da conflito
+}).min(7).max(8);
 
 const updateProductSchema = Joi.object({
   name: Joi.string().min(1).max(100).optional(),
@@ -20,9 +20,9 @@ const updateProductSchema = Joi.object({
   code: Joi.number().min(1).max(9223372036854775807).optional(),
   categoryId: Joi.string().length(24).hex().optional(),
   quantity: Joi.number().min(1).max(65535).optional(),
-  status: Joi.boolean().optional(),
+  status: Joi.boolean().optional(),  //se no Dto e o Entity é obrigatório mas o default é true como faz?
   deleted: Joi.boolean().optional()
-})
+}).min(1).max(9); //mesma duvida a cerca da quantidade de requisições (no caso de deleted como que o front manda? vai mandar SÓ ele ou o resto?)
 module.exports = { 
   createProductSchema, 
   updateProductSchema };
