ff5f63521b68c2a06d4551561912c1411869ee31
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-07-29 11:43:14 -0300

    refactor(Modulos): Enxuguei a quantidade de pastas e ja modifiquei os caminhos

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,10 +1,19 @@
 // routes/publicRoutes.js
 const express = require('express');
+<<<<<<< Updated upstream
 const paymentConditionRoutes = require('../modules/pagamento/payment_condition/payment_conditionRouter');
 const paymentRoutes = require("../modules/pagamento/payment/paymentRouter");
 const categoryRoutes = require('../modules/produto/category/categoryRouter');
 const siteRoutes = require('../modules/empresa/site/siteRouter');
 const enterpriseRoutes = require('../modules/empresa/enterprise/enterpriseRouter');
+=======
+const paymentConditionRoutes = require('../modules/payment_condition/payment_conditionRouter');
+const paymentRoutes = require("../modules/payment/paymentRouter");
+const categoryRoutes = require('../modules/category/categoryRouter');
+const productRoutes = require('../modules/product/productRouter');
+const siteRoutes = require('../modules/site/siteRouter');
+const enterpriseRoutes = require('../modules/enterprise/enterpriseRouter');
+>>>>>>> Stashed changes
 
 const publicRouter = express.Router();
 
