6d2707a984b6d61f265184f59382779f9d1e7020
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-08-19 10:58:28 -0300

    feat(stock_movementService): Criar lógica de criação e gets - Falta criar a lógica de relacionamentos, no caso de categoria de estoque e atualização de estoque dos produtos.

---
+++ src/modules/stock_movement/stock_movementService.js
@@ -0,0 +1,69 @@
+//stock_movementService.js
+const StockMovement = require("./stock_movementEntity");
+const StockCategory = require("../stock_category/stock_categoryEntity");
+
+//Create
+const createStockMovement = async function(data) {
+  try {
+    //validando a categoria de estoque
+    const stockCategory = await StockCategory.findById(data.stockCategoryId);
+    if (!stockCategory) {
+        throw new Error('Categoria de estoque não encontrada');
+    }
+
+    //validando os produtos
+    const products = await Product.find({_id: {$in: data.products.map(product => product.productId)}});
+    if (products.length !== data.products.length) {
+        throw new Error('Produto não encontrado');
+    }
+
+    //validando o administrador
+    const admin = await User.findById(data.adminId);
+    if (!admin) {
+        throw new Error('Administrador não encontrado');
+    }
+
+    //função para adicionar a movimentação na categoria de estoque
+    //CRIAR
+
+    const stockMovement = new StockMovement(data);//cria um novo
+    const stockMovementSaved = await stockMovement.save(); //salva no banco
+
+    //função para atualizar o estoque dos produtos
+    //CRIAR
+
+    return stockMovementSaved; //retorna o movimento de estoque salvo
+  }catch (error) {
+    throw new Error(`Erro ao criar movimento de estoque ${error.message}`);
+  }
+};
+
+
+//Read
+//All
+const getAllStockMovements = async function() {
+  try {
+    return await StockMovement.sort({name: 1});
+  }catch (error) {
+    throw new Error(`Erro ao buscar todos os movimentos de estoque: ${error.message}`);
+  }
+};
+
+const getStockMovementById = async function(id) {
+  try {
+    const stockMovement = await StockMovement.findById(id);
+    if (!stockMovement) {
+      throw new Error('Movimento de estoque não encontrado');
+    }
+  }
+  catch (error) {
+    throw new Error(`Erro ao buscar movimento de estoque: ${error.message}`);
+  }
+};
+
+
+module.exports = {
+    createStockMovement,
+    getAllStockMovements,
+    getStockMovementById
+};
