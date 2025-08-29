4273088cdf67e29e26557e9ab6e1c87c711c6d07
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-08-29 08:47:12 -0300

    feat(Category: Dto Controller): Tratamento de erros

--- src/modules/category/categoryDto.js
+++ src/modules/category/categoryDto.js
@@ -1,17 +1,43 @@
 const Joi = require('joi');
 
 const createCategorySchema = Joi.object({
-  name: Joi.string().trim().min(3).max(50).required(),
+  name: Joi.string()
+  .trim()
+  .min(3)
+  .max(50)
+  .required()
+  .messages({
+    'string.empty': 'Nome é obrigatório',
+    'string.min': 'Nome deve ter no mínimo 3 caracteres',
+    'string.max': 'Nome deve ter no máximo 50 caracteres'
+  }),
 
-  status: Joi.boolean().default(true).optional(),
+  status: Joi.boolean()
+  .default(true)
+  .optional(),
 }).min(1).max(2);
 
 const updateCategorySchema = Joi.object({
-  name: Joi.string().trim().min(3).max(50).optional(),
+  name: Joi.string()
+  .trim()
+  .min(3)
+  .max(50)
+  .optional()
+  .messages({
+    'string.min': 'Nome deve ter no mínimo 3 caracteres',
+    'string.max': 'Nome deve ter no máximo 50 caracteres'
+  }),
 
   productsList: Joi.array().items(
     Joi.object({
-      productId: Joi.string().length(24).hex().required()
+      productId: Joi.string()
+      .length(24)
+      .hex()
+      .required()
+      .messages({
+        'string.length': 'Produto deve ter exatamente 24 caracteres',
+        'string.hex': 'Produto deve ser um hexadecimal válido'
+      })
     })
   ).optional(),
 
