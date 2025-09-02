9fad015cf86ae065e5778723cf94e365ab5bd73d
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-09-02 17:43:41 -0300

    fix(User: bcrypt): Instalação do módulo e converter hash para senha

--- src/modules/user/userUtils.js
+++ src/modules/user/userUtils.js
@@ -4,12 +4,24 @@ const bcrypt = require("bcrypt");
 const convertPasswordToHash = async function(password) {
   try {
     const hash = await bcrypt.hash(password, 10);
+
     return hash;
   } catch (error) {
     throw new Error(`Erro ao converter senha para hash: ${error.message}`);
   };
 };
 
+const convertHashToPassword = async function(hash) {
+  try {
+    const password = await bcrypt.compare(hash, password);
+
+    return password;
+  } catch (error) {
+    throw new Error(`Erro ao converter hash para senha: ${error.message}`);
+  };
+};
+
 module.exports = {
-    convertPasswordToHash
+    convertPasswordToHash,
+    convertHashToPassword
 };
