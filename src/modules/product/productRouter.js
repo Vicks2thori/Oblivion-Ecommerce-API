3a35cf052ece2067f0a7542df7f01a9c4f4c358a
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-09-02 08:59:37 -0300

    refactor(Product): Alterações para legibilidade do código

--- src/modules/product/productRouter.js
+++ src/modules/product/productRouter.js
@@ -2,25 +2,18 @@
 const express = require('express');
 const productController = require('./productController');
 
-//ROUTER PÚBLICO (E-commerce)
-const publicRouter = express.Router();
 
-//Só rotas que o público pode acessar
-publicRouter.get('/active', productController.getActive);  // Novamente a duvida se faz sentido criar uma rota
-//No caso se ele fosse acessado dentro da categoria posso fazer por lá
-
-
-// ROUTER PRIVADO (Admin)
+//ADMIN
 const privateRouter = express.Router();
 
-// CRUD completo para admin
-privateRouter.post('/', productController.create);           // Criar
-privateRouter.get('/', productController.getAll);            // Listar todas
-privateRouter.get('/:id', productController.getById);        // Buscar por ID
-privateRouter.put('/:id', productController.update);         // Atualizar
-privateRouter.delete('/:id', productController.deleteProduct); // Deletar
+//Create -post
+privateRouter.post('/', productController.create);
 
-module.exports = {
-  public: publicRouter,
-  private: privateRouter
-};
+//Read -get
+privateRouter.get('/:id', productController.getById);
+
+//Update/Delete -put
+privateRouter.put('/:id', productController.update);
+privateRouter.put('/:id/delete', productController.deleteProduct);
+
+module.exports = { private: privateRouter };
--- src/modules/product/productService.js
+++ src/modules/product/productService.js
@@ -1,48 +1,26 @@
 //productService.js
 const Product = require("./productEntity");
 
-//CRUD
 
-//Create
+//CREATE
 const createProduct = async function(data) {
   try { 
-    const product = new Product(data); //cria um novo
-    return await product.save(); //salva no banco
-  }catch (error) {
+    const product = new Product(data);
+
+    return await product.save();
+  } catch (error) {
     throw new Error(`Erro ao criar produto: ${error.message}`);
-  }
+  };
 };
 
 
-//Read
-//All
-const getAllProducts = async function() {
-  try {
-    return await Product.find({deleted: false}).sort({name: 1});
-  }catch (error) {
-    throw new Error(`Erro ao buscar todos os produtos: ${error.message}`);
-  }
-};
-
-//Active
-const getActiveProducts = async function() { //faz sentido se eu relacionar na categoria?
-  try {
-    return await Product.find({
-      deleted: false,
-      status: true 
-    }).sort({ name: 1 });
-  }catch (error) {
-    throw new Error(`Erro ao buscar produtos ativos: ${error.message}`);
-  }
-};
-
-//By ID
+//READ
 const getProductById = async function(id) {
   try {
     const getById = await Product.findById(id);
     
-    if (!getById || getById.deleted) { //se não encontrou ou encontrou e esta deletada
-      throw new Error('Produto não encontrado'); //cria um novo erro
+    if (!getById || getById.deleted) {
+      throw new Error('Produto não encontrado');
     }
     
     return getById;
@@ -52,48 +30,55 @@ const getProductById = async function(id) {
 };
 
 
-//Update
+//UPDATE
 const updateProduct = async function(id, updateData) {
   try {
-    const updated = await Product.findOneAndUpdate(
-      {_id: id, deleted: false }, //só atualiza se não foi deletado
-      updateData, 
-      {new: true, runValidators: true})
+    const product = await Product.findById(id);
     
-    if (!updated) {
-      throw new Error('Produto não encontrado'); //novo erro caso não encontre
-    }
+    if (!product || product.deleted) {
+      throw new Error('Produto não encontrado');
+    };
+
+    if (updateData.categoryId) {
+      await addProductToCategoryWithTransfer(product._id, updateData.categoryId);
+    };
+
+    Object.assign(product, updateData);
     
-    return updated;
+    return await product.save();
   } catch (error) {
     throw new Error(`Erro ao atualizar produto: ${error.message}`);
-  }
+  };
 };
 
 
-//Delete (soft delete)
+//DELETE
 const deleteProduct = async function(id) {
  try {
+    const product = await Product.findById(id);
+    
+    if (!product || product.deleted) {
+      throw new Error('Produto não encontrado');
+    };
+
     const deleted = await Product.findOneAndUpdate(
       {_id: id, deleted: false},
-      {deleted: true},
+      {deleted: true},  
       {new: true}
     );
     
     if (!deleted) {
       throw new Error('Produto não encontrado');
-    }
+    };
     
     return deleted;
   } catch (error) {
     throw new Error(`Erro ao deletar produto: ${error.message}`);
-  }
+  };
 };
 
 module.exports = {
     createProduct,
-    getAllProducts,
-    getActiveProducts,
     getProductById,
     updateProduct,
     deleteProduct
