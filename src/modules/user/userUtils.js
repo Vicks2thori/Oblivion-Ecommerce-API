a67af9e767b8b38bd65ad88e5582efad4fedce57
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-09-03 08:27:51 -0300

    feat(User: Hash de dados): Criptografia de dados e validação de cadastro -UserEntity:   Password tamanho 60 por conta do bcrypt   Email, cpf e cell para 64 por conta do sha256 -UserUtils:   Comparar senha   Converter dados em hash pesquisaveis (para validação de unicidade no banco optei o sha256)   Checar se email, cpf e cell ja existem no banco -UserService:   Create e Update(client e admin): Adicionado validação de unicidade e criptografia de dados

--- src/modules/user/userUtils.js
+++ src/modules/user/userUtils.js
@@ -1,27 +1,83 @@
-//userUtills.js
+//userUtils.js
 const bcrypt = require("bcrypt");
+const crypto = require('crypto');
+const User = require("./userEntity");
 
 const convertPasswordToHash = async function(password) {
   try {
     const hash = await bcrypt.hash(password, 10);
-
     return hash;
   } catch (error) {
     throw new Error(`Erro ao converter senha para hash: ${error.message}`);
   };
 };
 
-const convertHashToPassword = async function(hash) {
+const comparePasswordHash = async (password, hash) => {
   try {
-    const password = await bcrypt.compare(hash, password);
-
-    return password;
+    return await bcrypt.compare(password, hash);
   } catch (error) {
-    throw new Error(`Erro ao converter hash para senha: ${error.message}`);
+    throw new Error(`Erro ao verificar senha: ${error.message}`);
+  };
+};
+
+const convertDataToSearchableHash = function(data) {
+  try {
+    return crypto.createHash('sha256').update(data).digest('hex');
+  } catch (error) {
+    throw new Error(`Erro ao converter dados para hash: ${error.message}`);
+  };
+};
+
+const checkIfEmailExist = async function(email) {
+  try {
+    const emailHash = convertDataToSearchableHash(email);
+    const emailExists = await User.findOne({deleted: false, email: emailHash});
+
+    if (!emailExists) {
+      return false;
+    };
+
+    return true;
+  } catch (error) {
+    throw new Error(`Erro ao verificar email: ${error.message}`);
+  };
+};
+
+const checkIfCpfExist = async function(cpf) {
+  try {
+    const cpfHash = convertDataToSearchableHash(cpf);
+    const cpfExists = await User.findOne({deleted: false, 'clientDetails.cpf': cpfHash});
+    
+    if (!cpfExists) {
+      return false;
+    };
+
+    return true;
+  } catch (error) {
+    throw new Error(`Erro ao verificar CPF: ${error.message}`);
+  };
+};
+
+const checkIfCellExist = async function(cell) {
+  try {
+    const cellHash = convertDataToSearchableHash(cell);
+    const cellExists = await User.findOne({deleted: false, 'clientDetails.cell': cellHash});
+
+    if (!cellExists) {
+      return false;
+    };
+
+    return true;
+  } catch (error) {
+    throw new Error(`Erro ao verificar telefone: ${error.message}`);
   };
 };
 
 module.exports = {
     convertPasswordToHash,
-    convertHashToPassword
+    comparePasswordHash,
+    convertDataToSearchableHash,
+    checkIfEmailExist,
+    checkIfCpfExist,
+    checkIfCellExist
 };
