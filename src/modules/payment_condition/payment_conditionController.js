e9df7cd097aed621a13b06e6663f2f21e4dc1a06
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-09-04 00:08:58 -0300

    refactor(PaymentConditions): Aplicação de boas práticas para legibilidade e alteração de lógica de rotas

--- src/modules/payment_condition/payment_conditionController.js
+++ src/modules/payment_condition/payment_conditionController.js
@@ -1,37 +1,31 @@
 //payment_conditionController.js
 const { createPaymentConditionSchema, updatePaymentConditionSchema } = require('./payment_conditionDto');
 const PaymentCondition = require('./payment_conditionService');
-//const { badRequest400, responseHelpersOk, responseHelpersError } = require("../../../routes/responseHelpers"); futuramente qando estiver funcionando
 
-//CRUD
 
-//Create
+//CREATE
 async function create(req, res) {
   try {
-    //Validar DTO
-    const { error, value } = createPaymentConditionSchema.validate(req.body); //validate do Joi retorna um erro(null se estiver ok) e os valores
+    const { error, value } = createPaymentConditionSchema.validate(req.body);
+
     if (error) {
-      //400 - Dados inválidos
+      //Dados inválidos
       return res.status(400).json({
         success: false,
-        message: '400 - Dados inválidos',
-        errors: error.details.map(d => d.message) //extrai só as mensagens
+        message: `400 - Dados inválidos: ${error.details.map(d => d.message)}`
       });
     };
 
-    //Criar através do Service
     const paymentCondition = await PaymentCondition.createPaymentCondition(value);
 
-    //200 - Sucesso geral
+    //Sucesso
     res.status(200).json({
       success: true,
       message: '200 - Operação realizada com sucesso',
       data: paymentCondition
     });
-
-
-  }catch (error) {
-    //500 - Erro interno do servidor
+  } catch (error) {
+    //Erro interno do servidor
     return res.status(500).json({
       success: false,
       message: error.message
@@ -40,15 +34,12 @@ async function create(req, res) {
 };
 
 
-//Read
-
-//All
+//READ
 async function getAll(req, res) {
   try {
     const paymentCondition = await PaymentCondition.getAllPaymentConditions();
 
-    //OK
-    //200 - Sucesso geral
+    //Sucesso
     res.status(200).json({
       success: true,
       message: '200 - Operação realizada com sucesso',
@@ -56,42 +47,21 @@ async function getAll(req, res) {
     });
 
   } catch (error) {
-    //500 - Erro interno do servidor
+    //Erro interno do servidor
     return res.status(500).json({
       success: false,
       message: error.message
     });
-  }
-}
-
-//Active
-async function getActive(req, res) {
-  try {
-    const activeConditions = await PaymentCondition.getActivePaymentConditions();
-
-    //200 - Sucesso geral
-    res.status(200).json({
-      success: true,
-      message: '200 - Operação realizada com sucesso',
-      data: activeConditions
-    });
-
-  }catch (error) {
-    //500 - Erro interno do servidor
-    return res.status(500).json({
-      success: false,
-      message: error.message
-    });
-  }
+  };
 };
 
-//By ID
+
 async function getById(req, res) {
   try {
     const { id } = req.params;
     const paymentCondition = await PaymentCondition.getPaymentConditionById(id);
 
-    //200 - Sucesso geral
+    //Sucesso geral
     res.status(200).json({
       success: true,
       message: '200 - Operação realizada com sucesso',
@@ -99,7 +69,7 @@ async function getById(req, res) {
     });
 
   }catch (error) {
-    //500 - Erro interno do servidor
+    //Erro interno do servidor
     return res.status(500).json({
       success: false,
       message: error.message 
@@ -108,43 +78,44 @@ async function getById(req, res) {
 };
 
 
-//Update
+//UPDATE
 async function update(req, res) {
   try {
     const { id } = req.params;
 
     const { error, value } = updatePaymentConditionSchema.validate(req.body);
+
     if (error) {
+      //Dados inválidos
       return res.status(400).json({
         success: false,
-        errors: error.details.map(d => d.message) // ✅ Só aqui usar .details
+        message: `400 - Dados inválidos: ${error.details.map(d => d.message)}`
       });
-    }
+    };
     
-    const result = await PaymentCondition.updatePaymentCondition(id, value);
+    const paymentCondition = await PaymentCondition.updatePaymentCondition(id, value);
     
-    //200 - Sucesso geral
+    //Sucesso
     return res.status(200).json({
       success: true,
-      data: result
+      data: paymentCondition
     });
-    
   } catch (error) {
-    //500 - Erro interno do servidor
+    //Erro interno do servidor
     return res.status(500).json({
       success: false,
       message: error.message
     });
-  }
-}
+  };
+};
 
-//Delete
+//DELETE
 async function deletePaymentCondition(req, res) {
   try {
     const { id } = req.params;
     const deleted = await PaymentCondition.deletePaymentCondition(id);
 
-    //200 - Sucesso geral
+    //Sucesso
     res.status(200).json({
       success: true,
       message: '200 - Operação realizada com sucesso',
@@ -152,18 +123,17 @@ async function deletePaymentCondition(req, res) {
     });
 
   } catch (error) {
-    //500 - Erro interno do servidor
+    //Erro interno do servidor
     return res.status(500).json({
       success: false,
       message: error.message
     });
-  }
-}
+  };
+};
 
 module.exports = {
   create,
   getAll,
-  getActive,
   getById,
   update,
   deletePaymentCondition
--- src/modules/payment_condition/payment_conditionDto.js
+++ src/modules/payment_condition/payment_conditionDto.js
@@ -1,19 +1,30 @@
 //payment_conditionDto.js
-//Se for manipulada diretamente (upload de imagens fora do produto)
 const Joi = require('joi');
 
 const createPaymentConditionSchema = Joi.object({
-  name: Joi.string().min(2).max(50).required(),
-  status: Joi.boolean().default(true).required(),
-  //deleted não vem do front é controlado no back (pelo menos na hora de criar)
-}).min(2).max(2);
+  name: Joi.string()
+  .min(2)
+  .max(50)
+  .required(),
+
+  status: Joi.boolean()
+  .default(true)
+}).min(1).max(2);
 
 const updatePaymentConditionSchema = Joi.object({
-  name: Joi.string().min(2).max(50).optional(),
-  status: Joi.boolean().optional(),
-  deleted: Joi.boolean().optional()
+  name: Joi.string()
+  .min(2)
+  .max(50)
+  .optional(),
+
+  status: Joi.boolean()
+  .optional(),
+
+  deleted: Joi.boolean()
+  .optional()
 }).min(1).max(3);
 
 module.exports = { 
-  createPaymentConditionSchema, 
-  updatePaymentConditionSchema };
+  createPaymentConditionSchema,
+  updatePaymentConditionSchema
+};
