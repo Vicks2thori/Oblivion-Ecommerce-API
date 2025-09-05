afad57988f4320c592b71c807d6fb24d80b61f73
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-09-05 12:29:33 -0300

    styles(StockCategory): Aplicação de boas práticas para legibilidade

--- src/modules/stock_category/stock_categoryEntity.js
+++ src/modules/stock_category/stock_categoryEntity.js
@@ -2,31 +2,32 @@
 const mongoose = require('mongoose');
 
 const StockCategorySchema = new mongoose.Schema({
-  name: { 
-    type: String, 
-    required: [true, 'Nome é obrigatório'], //required + mensagem personalizada
-    trim: true,  //Remove espaços inicio/fim
-    minlength: [3, 'Nome deve ter um minímo de 3 caracteres'],
-    maxlength: [50, 'Nome deve ter um máximo de 50 caracteres']
-    //unique: true, //Quando o unique esta ativo ele retorna um erro, mesmo quando o item foi "deletado"
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 50
   },
-  status: { 
+
+  status: {
     type: Boolean,
     required: true,
-    default: true 
+    default: true
   },
+
+  //Soft delete
   deleted: {
     type: Boolean,
     required: true,
     default: false
   }
-}, { 
-  timestamps: true, //controle automático de tempo
-  versionKey: false //remove campo inutil
+}, {
+  timestamps: true,
+  versionKey: false
 });
 
-//indexação para performance
-StockCategorySchema.index({name: 1})
-StockCategorySchema.index({status: 1, deleted: 1}) //melhorar
+StockCategorySchema.index({name: 1});
+StockCategorySchema.index({status: 1, deleted: 1});
 
 module.exports = mongoose.model('StockCategory', StockCategorySchema);
