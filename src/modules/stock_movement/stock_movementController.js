56fbce482eafe1b7cfc84d45fcb7736f34b04dda
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-09-10 00:04:32 -0300

    style(stock_movementController): Aplicação de boas práticas para legibilidade

--- src/modules/stock_movement/stock_movementController.js
+++ src/modules/stock_movement/stock_movementController.js
@@ -3,9 +3,19 @@ const stockMovementService = require('./stock_movementService');
 const { createStockMovementSchema } = require('./stock_movementDto');
 
 
+//CREATE
 const create = async function(req, res) {
   try {
-    const stockMovement = await stockMovementService.createStockMovementSchema(req.body);
+    const { error, value } = createStockMovementSchema.validate(req.body);
+    if (error) {
+      //Dados inválidos
+      return res.status(400).json({
+        success: false,
+        message: `400 - Dados inválidos: ${error.details.map(d => d.message)}`
+      });
+    };
+
+    const stockMovement = await stockMovementService.createStockMovement(value);
     
     res.status(200).json({
       success: true,
@@ -17,11 +27,11 @@ const create = async function(req, res) {
       success: false,
       message: error.message
     });
-  }
+  };
 };
 
-//Read
-//Get All
+
+//READ
 const getAll = async function(req, res) {
   try {
     const stockMovements = await stockMovementService.getAllStockMovements();
@@ -36,10 +46,9 @@ const getAll = async function(req, res) {
       success: false,
       message: error.message
     });
-  }
+  };
 };
 
-//Get By Id
 const getById = async function(req, res) {
     try {
       const stockMovement = await stockMovementService.getStockMovementById(req.params.id);
@@ -54,7 +63,7 @@ const getById = async function(req, res) {
         success: false,
         message: error.message
       });
-    }
+    };
   };
 
 module.exports = {
